04947ed978a907772c835b322aa9003897ebe716
zyliaz <zyliduck4758@gmail.com>
2025-09-18 23:02:36 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+install.packages("shiny"), dependencies = TRUE)
+
+IRkernel::installspec(user = FALSE)
